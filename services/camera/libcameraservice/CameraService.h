5d50ecfbe0cd24307d12a4bf658e1c3801c894e9
Eino-Ville Talvala <etalvala@google.com>
2012-01-05 15:34:53 -0800

    Switch camera sounds to always use the system enforced audio stream.
    Instead of picking between the music stream and the enforced audio
    stream, change the camera service to always play sounds through
    enforced system stream. Also update the currently-hidden CameraSound
    API to match.
    
    Bug: 5778365
    Change-Id: I3cc64b1d1ff567dbac8020a665d5b19846197ff3

--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -77,7 +77,6 @@ private:
     void                setCameraFree(int cameraId);
 
     // sounds
-    audio_stream_type_t mAudioStreamType;
     MediaPlayer*        newMediaPlayer(const char *file);
 
     Mutex               mSoundLock;
