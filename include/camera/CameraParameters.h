cf9c362773a8100fe35ce07ddfdcdcad5f281ad3
steppnasty <steppnasty@gmail.com>
2014-07-17 09:46:44 -0400

    libcamera_client: updated parameters

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -36,21 +36,6 @@ struct Size {
         height = h;
     }
 };
-#ifdef QCOM_HARDWARE
-struct FPSRange{
-    int minFPS;
-    int maxFPS;
-
-    FPSRange(){
-        minFPS=0;
-        maxFPS=0;
-    };
-    FPSRange(int min,int max){
-        minFPS=min;
-        maxFPS=max;
-   };
-};
-#endif
 class CameraParameters
 {
 public:
